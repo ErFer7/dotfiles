47029efedccbcd0a5dde29a51643dc596407c728
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2026-06-15 22:45:16 -0300

    Hyprland changes

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -5,14 +5,8 @@ return {
         opts = {
             picker = {
                 sources = {
-                    explorer = {
-                        hidden = true,
-                        ignored = true,
-                    },
-                    files = {
-                        hidden = true,
-                        ignored = false,
-                    },
+                    explorer = { hidden = true, ignored = true },
+                    files = { hidden = true, ignored = false },
                 },
             },
             styles = {
@@ -47,14 +41,43 @@ return {
                     "webm",
                     "pdf",
                 },
+                doc = {
+                    enabled = true,
+                    inline = false,
+                    float = true,
+                    max_width = 60,
+                    max_height = 30,
+                },
+                convert = {
+                    notify = true,
+                    magick = {
+                        default = { "{src}[0]", "-scale", "1280x720>" },
+                        vector = { "-density", 192, "{src}[0]" },
+                        math = { "-density", 192, "{src}[0]", "-trim" },
+                        pdf = { "-density", 192, "{src}[0]", "-background", "white", "-alpha", "remove", "-trim" },
+                    },
+                },
+                icons = {
+                    math = "󰪚 ",
+                    chart = "󰄧 ",
+                    image = " ",
+                },
+                math = {
+                    latex = {
+                        enabled = true,
+                    },
+                    typst = {
+                        enabled = true,
+                        tpl = [[
+        #set page(width: auto, height: auto, margin: (x: 2pt, y: 2pt))
+        #show math.equation.where(block: false): set text(top-edge: "bounds", bottom-edge: "bounds")
+        #set text(size: 12pt, fill: rgb("${color}"))
+        ${header}
+        ${content}]],
+                    },
+                },
             },
-            doc = {
-                enabled = true,
-                inline = false,
-                float = true,
-                max_width = 60,
-                max_height = 30,
-            },
+
             wo = {
                 wrap = false,
                 number = false,
@@ -63,31 +86,6 @@ return {
                 signcolumn = "no",
                 foldcolumn = "0",
                 statuscolumn = "",
-            },
-
-            convert = {
-                notify = true,
-                magick = {
-                    default = { "{src}[0]", "-scale", "1280x720>" },
-                    vector = { "-density", 192, "{src}[0]" },
-                    math = { "-density", 192, "{src}[0]", "-trim" },
-                    pdf = { "-density", 192, "{src}[0]", "-background", "white", "-alpha", "remove", "-trim" },
-                },
-            },
-
-            icons = {
-                math = "󰪚 ",
-                chart = "󰄧 ",
-                image = " ",
-            },
-
-            typst = {
-                tpl = [[
-        #set page(width: auto, height: auto, margin: (x: 2pt, y: 2pt))
-        #show math.equation.where(block: false): set text(top-edge: "bounds", bottom-edge: "bounds")
-        #set text(size: 12pt, fill: rgb("${color}"))
-        ${header}
-        ${content}]],
             },
         },
     },
