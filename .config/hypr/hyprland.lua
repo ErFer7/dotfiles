105038e1053ed75f73daa33e3776e90b3fc839a6
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2026-08-29 17:32:57 -0300

    Sync with desktop

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -9,9 +9,9 @@
 
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
-	output = "eDP-1",
-	mode = "1920x1080@59.99900",
-	position = "0x0",
+	output = "",
+	mode = "preferred",
+	position = "auto",
 	scale = 1,
 })
 
