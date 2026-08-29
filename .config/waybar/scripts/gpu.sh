2b6a3b22dec7b6542e0c9c00887fa4897f241cc9
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2026-08-29 18:52:40 -0300

    NVIDIA fix

--- .config/waybar/scripts/gpu.sh
+++ .config/waybar/scripts/gpu.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if ! command -v nvidia-smi &> /dev/null; then
+    echo ""
+    exit 1
+fi
+
 USAGE=$(printf "%0*d" 3 $(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits))
 
 echo "{\"text\":\"$USAGE\"}"
